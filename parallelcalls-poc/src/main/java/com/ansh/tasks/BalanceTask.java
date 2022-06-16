530652526b187e45d0066df27655ea81c64ba41a
ASUS <ASUS@LAPTOP-VI1QT254>
2022-06-16 13:03:39 +0530

    c

--- parallelcalls-poc/src/main/java/com/ansh/tasks/BalanceTask.java
+++ parallelcalls-poc/src/main/java/com/ansh/tasks/BalanceTask.java
@@ -8,14 +8,22 @@
  * You should have received a copy of the My license with
  * this file. If not, please write to: nikhilninawe.in1@gmail.com.
  */
- 
+
 package com.ansh.tasks;
+
+import java.util.concurrent.Callable;
+
+import com.ansh.model.TaskResult;
 
 /**
  * @author : bunty
-	@date : Jun 16, 2022
- *	
+ * @date : Jun 16, 2022
+ * 
  */
-public class BalanceTask {
+public class BalanceTask implements Callable<TaskResult> {
+
+	public TaskResult call() throws Exception {
+		return null;
+	}
 
 }
